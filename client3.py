f79be2abbdfb8112233bf067c88d61bdb5eff2a7
d-manas28 <dubey28.manas@gmail.com>
2023-09-16 20:21:46 +0530

    updated client3 file

--- client3.py
+++ client3.py
@@ -35,14 +35,20 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # updating price to average of bid and ask and rounding the value to three decimal places
+    price = round((bid_price + ask_price)/2,3)
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # updating the below code to return the ratio of the price_a to price_b
+    # Note that both of the price values could be 0. So we need to handle such cases
+
+    if(price_a  == 0 or price_b == 0):
+        return 0
+    return price_a/price_b
 
 
 # Main
@@ -52,8 +58,16 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        
+        # We need to store the stock prices in order to query them in getRatio to retrieve ratios between the stock values
+        # Lets store these in a HashMap or Python Dict, given below   
+        stock_price_map = {}
+
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            # Store the stock values against the stock name
+            stock_price_map[stock] = price
 
-        print("Ratio %s" % getRatio(price, price))
+        # Let's retrieve sample stock names from test.csv "DEF" and "ABC" and query them 
+        print("Ratio %s" % getRatio(stock_price_map["DEF"], stock_price_map["ABC"]))
